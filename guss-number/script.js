85c75f99aa64b1f6d6d0966e44da5f98b3d96c1c
Sergey Saprankov <sergey-saprankov@mail.ru>
2022-07-22 18:25:32 +0300

    added game guss-number

---
+++ guss-number/script.js
@@ -0,0 +1,71 @@
+'use strict';
+const body = document.body;
+const message = document.querySelector('.guess-message');
+const buttonCheck = document.querySelector('.check');
+const input = document.querySelector('.number-input');
+const question = document.querySelector('.question');
+const score = document.querySelector('.score');
+const bestResult = document.querySelector('.highscore');
+const restart = document.querySelector('.again');
+
+
+const randomNumber = () => Math.floor(Math.random() * 20) + 1;
+let secretNumber = randomNumber();
+
+restart.addEventListener('click', function() {
+  message.textContent = 'Начни угадывать';
+  input.value = '';
+  question.textContent = '???';
+  score.textContent = 20;
+  body.style.backgroundColor = '#000';
+})
+
+
+
+
+buttonCheck.addEventListener('click', function() {
+  const guessingNumber =  Number(input.value);
+  // Не введено число
+  if (!guessingNumber) {
+    message.textContent = 'Введите число!!!';
+  //Player won
+  } else if (guessingNumber === secretNumber) {
+    message.textContent = ('Правильно');
+    body.style.backgroundColor = 'green';
+    Number(bestResult.textContent) < Number(score.textContent) ? bestResult.textContent = score.textContent : bestResult.textContent;
+    question.textContent = secretNumber;
+    question.style.width = '50rem';
+    secretNumber = randomNumber();
+    //Если игрок близок к ответу
+  } else if (Math.abs(guessingNumber - secretNumber) === 1) {
+    message.textContent = ('Горячо');
+    if (Number(score.textContent) > 1) {
+      score.textContent = Number(score.textContent) - 1;
+    } else {
+      message.textContent = 'Game over';
+      body.style.backgroundColor = 'red';
+      score.textContent = 0;
+    }   
+  } else if (guessingNumber > secretNumber) {
+    message.textContent = ('Слишком большое число');
+    if (Number(score.textContent) > 1) {
+      score.textContent = Number(score.textContent) - 1;
+    } else {
+      message.textContent = 'Game over';
+      body.style.backgroundColor = 'red';
+      score.textContent = 0;
+    }
+  } else if (guessingNumber < secretNumber) {
+    message.textContent = ('Слишком маленькое число');
+    if (Number(score.textContent) > 1) {
+      score.textContent = Number(score.textContent) - 1;
+    } else {
+      message.textContent = 'Game over';
+      body.style.backgroundColor = 'red';
+      score.textContent = 0;
+    }
+  } 
+})
+
+
+
