066d398c8dab62383305a5a6cdc43efd858d7231
Sergey Saprankov <sergey-saprankov@mail.ru>
2022-07-31 16:00:31 +0300

    work date

---
+++ date/script.js
@@ -0,0 +1,2 @@
+const now = new Date('2022-07-29');
+console.log(now);
--- food/js/script.js
+++ food/js/script.js
@@ -77,17 +77,68 @@ tabsParent.addEventListener('click', (evt) => {
   }
 })
 
-
-
-
-
-
-
-
-
-
 hideTabsContent();
 showTabsContent();
+
+
+
+
+
+
+
+//Timer
+
+  const deadline = '2022-08-1';
+
+  const getTimeRemaining = (endtime) => {
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endtime) - Date.parse(new Date());
+
+    if (t < 0) {
+      [days, hours, minutes, seconds] = [0, 0, 0, 0];
+    } else {
+      days = Math.floor(t / (1000 * 60 * 60 * 24));
+
+     hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+
+     minutes = Math.floor((t / (1000 * 60)) % 60);
+
+     seconds = Math.floor((t / 1000) % 60);
+    }
+     
+    return {total: t, days, hours, minutes, seconds};
+  }
+
+  const getZero = (num) => {
+    return num >= 0 && num < 10 ? `0${num}` : num;
+  }
+
+  const setClock = (selector, endtime) => {
+    const timer = document.querySelector(selector);
+    const days = timer.querySelector('#days');
+    const hours = timer.querySelector('#hours');
+    const minutes = timer.querySelector('#minutes');
+    const seconds = timer.querySelector('#seconds');
+
+    updateClock();
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+      days.textContent = getZero(t.days);
+      hours.textContent = getZero(t.hours);
+      minutes.textContent = getZero(t.minutes);
+      seconds.textContent = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+
+    const timeInterval = setInterval(updateClock, 1000);
+  }
+
+
+  setClock('.timer', deadline);
 
 
 })
